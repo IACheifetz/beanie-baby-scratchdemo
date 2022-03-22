322a650512d37b187981e353597a748f57579703
Ian Cheifetz <sylvan.iacheifetz09@gmail.com>
2022-03-22 16:53:45 -0700

    added async functions for fetch util js

---
+++ fetch-utils.js
@@ -0,0 +1,21 @@
+const SUPABASE_URL = 'https://piaaxjtzsxubseeoqown.supabase.co';
+const SUPABASE_KEY = const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBpYWF4anR6c3h1YnNlZW9xb3duIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NDc5ODgwOTMsImV4cCI6MTk2MzU2NDA5M30.-JF8rp3uujkLpMpiJj70fwmuyamVh64NHTUYK5UFA04';
+const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+
+export async function getAllCharacters() {
+    const response = await client
+    .from('MetalGearSolid')
+    .select();
+    
+    return response.data;
+}
+
+export async function getCharacter() {
+    const response = await client
+    .from('MetalGearSolid')
+    .select()
+    .match({id: id})
+    .single();
+
+    return response.data;
+}
