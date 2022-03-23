3ce7cbd215acc14a080cc04de8bfee456fb2d995
Ian Cheifetz <sylvan.iacheifetz09@gmail.com>
2022-03-22 17:09:52 -0700

    added eventlistener to app js and began on details html and js

--- app.js
+++ app.js
@@ -1,8 +1,33 @@
 // import functions and grab DOM elements
-
+import { getAllCharacters } from './fetch-utils.js';
 // let state
 
-// set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+const charsEl = document.querySelector('.characters');
+
+window.addEventListener('load', async () => {
+    const chars = await getAllCharacters();
+
+    for (let char of chars) {
+        const charEl = document.createElement('a');
+        const nameEl = document.createElement('p');
+        const AgeEl = document.createElement('p');
+        const DescriptionEl = document.createElement('p');
+        const AffiliationEl = document.createElement('ul');
+        const AppearancesEl = document.createElement('ul');
+
+        charEl.classList.add('char');
+        charEl.href = `./MetalGearSolid/?id=${char.id}`; 
+        nameEl.textContent = char.name;
+        DescriptionEl.textContent = char.description;
+
+        // for (let tag of tags) {
+        //     const li = document.createElement('li');
+
+        //     li.textContent = tag;
+        //     AffiliationEl.appendChild(li);
+        // }
+
+        charEl.append(nameEl, AgeEl, DescriptionEl, AffiliationEl, AppearancesEl);
+        charsEl.append(charEl);
+    }
+});
---
+++ details/details.js
@@ -0,0 +1,1 @@
+import { getCharacter } from '../fetch-utils.js';
